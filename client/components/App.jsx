d3911a0d09b5b8ce284f71878b91d4ec5cd1e470
Jonathan Lau <jonhlau28@gmail.com>
2018-12-09 22:10:16 -0800

    webpack update

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,7 @@
 import React, { Fragment } from "react"
+import ReactDOM from "react-dom"
+import axios from "axios"
+
 import Map from "./map/map_component"
 
 export default class App extends React.Component {
