c2dbd5b00cd78e5083ef5e21e0cfb7713cce1e21
Andre <25297095+xvzero@users.noreply.github.com>
2018-12-14 23:39:22 -0800

    fix webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const config = {
 
 module.exports = {
   mode: config.mode,
-  entry: ["babel-polyfill", "./client/resources"],
+  entry: ["babel-polyfill", "./client"],
   output: {
     path: path.join(__dirname, "client/resources"),
     filename: "bundle.js"
