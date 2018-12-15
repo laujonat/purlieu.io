78b8da274eee6aeaf7090f2bdda0f3c4ad010183
Andre <25297095+xvzero@users.noreply.github.com>
2018-12-15 15:09:38 -0800

    working code

--- client/components/InputForm/index.jsx
+++ client/components/InputForm/index.jsx
@@ -1,15 +1,16 @@
 import React, { Component, Fragment } from "react"
+import PropTypes from 'prop-types';
 import { WELCOME_DESCRIPTION } from "../../assets/loading/constants"
 
 export default class InputForm extends Component {
   constructor(props) {
     super(props)
 
-    const addressInput = this.props.currentAddress
     this.state = {
       dollarInput: "",
-      addressInput: addressInput,
+      addressInput: this.props.currentAddress,
       formSubmitted: false,
+      isButtonEnabled: true
     }
   }
 
@@ -19,48 +20,46 @@ export default class InputForm extends Component {
     }
   }
 
-  changeFormState = () => {
-    this.refs.btn.removeAttribute("disabled")
+  onSubmitClicked = e => {
+    e.preventDefault();
+    this.setState({
+      formSubmitted: true
+    }, () => {
+      const { onSubmit } = this.props; 
+      const { dollarInput, addressInput } = this.state;
+  
+      onSubmit ? onSubmit(dollarInput, addressInput) : null
+    })
   }
 
-  submitForm = e => {
-    e.preventDefault()
-    this.refs.btn.setAttribute("disabled", "disabled")
-    const { drawBoundaries } = this.props; 
-    const { dollarInput, addressInput } = this.state;
-
-    drawBroundaries ? drawBroundaries(dollarInput, addressInput) : null
-  }
-
-  updateInput = field => e => {
-    this.setState({ [field]: e.target.value })
+  onInputChange = field => e => {
+    this.setState({ [field]: e.target.value }, this.validateInputs);
   }
 
   validateDollar = amt => {
-    const regex = /^\$?[0-9]+(\.[0-9][0-9])?$/
-    const bound = amt >= 9.99 && amt <= 400
-    return regex.test(amt) && bound
+    const regex = /^\$?[0-9]+(\.[0-9][0-9])?$/;
+    const bound = amt >= 9.99 && amt <= 400;
+    return regex.test(amt) && bound;
   }
 
-  validate = () => {
-    const { dollarInput, addressInput } = this.state
-    let checkValidDollar = this.validateDollar(dollarInput)
-    return checkValidDollar && addressInput.length > 0
+  validateInputs = () => {
+    const { dollarInput, addressInput } = this.state;
+    return this.validateDollar(dollarInput) && (addressInput && addressInput.length > 0);
   }
 
   render() {
     if (!this.props.currentAddress) return null
-    const isEnabled = this.validate()
+    const { dollarInput, addressInput, isButtonEnabled } = this.state;
 
-    let formClassName, formName, infoContainer
+    let formClassName, formName, infoContainer;
 
     if (this.state.formSubmitted) {
-      formName = "submitted"
-      formClassName = "user-submitted-form"
+      formName = 'submitted'
+      formClassName = 'user-submitted-form'
     } else {
-      formClassName = "user-input-form"
+      formClassName = 'user-input-form'
       infoContainer = (
-        <div className="info-container">
+        <div className='info-container'>
           <p>{WELCOME_DESCRIPTION}</p>
         </div>
       )
@@ -69,52 +68,61 @@ export default class InputForm extends Component {
     return (
       <Fragment>
         <form className={formClassName}>
-          <div id={formName} className="question">
+          <div id={formName} className='question'>
             WHERE CAN I GO WITH
           </div>
 
-          <div id={formName} className="dollar-input-div">
+          <div id={formName} className='dollar-input-div'>
             <img
-              className="dollar-input-icon"
-              src="https://i.imgur.com/um4yd7D.png"
+              className='dollar-input-icon'
+              src='https://i.imgur.com/um4yd7D.png'
             />
             <input
-              type="number"
+              type='number'
               id={formName}
-              className={`dollar-input`}
-              value={this.state.dollarInput}
-              onChange={this.updateInput("dollarInput")}
+              className='dollar-input'
+              value={dollarInput}
+              onChange={this.onInputChange('dollarInput')}
             />
           </div>
 
-          <div id={formName} className="question">
+          <div id={formName} className='question'>
             &nbsp;FROM&nbsp;
           </div>
 
-          <div id={formName} className="address-input-div">
+          <div id={formName} className='address-input-div'>
             <img
-              className="address-input-icon"
-              src="https://i.imgur.com/UFHf4wX.png"
+              className='address-input-icon'
+              src='https://i.imgur.com/UFHf4wX.png'
             />
             <input
-              type="text"
+              type='text'
               id={formName}
-              className={`address-input`}
-              value={this.state.addressInput}
-              onChange={this.updateInput("addressInput")}
+              className='address-input'
+              value={addressInput}
+              onChange={this.onInputChange('addressInput')}
             />
           </div>
 
           <button
             id={formName}
-            disabled={!isEnabled}
-            className="submit"
-            ref="btn"
-            onClick={this.submitForm}
+            disabled={!isButtonEnabled}
+            className='submit'
+            onClick={this.onSubmitClicked}
           />
           {infoContainer}
         </form>
       </Fragment>
     )
   }
+}
+
+InputForm.propTypes = {
+  currentAddress: PropTypes.string,
+  onSubmit: PropTypes.func
+};
+
+InputForm.defaultProps = {
+  currentAddress: null,
+  onSubmit: null
 }
