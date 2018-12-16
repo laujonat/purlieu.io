f0cd21814ded94bf4c0f34a56446e3c6c6007bc9
Andre <25297095+xvzero@users.noreply.github.com>
2018-12-15 18:48:08 -0800

    Fix export problem in loading

--- client/components/Loading/index.jsx
+++ client/components/Loading/index.jsx
@@ -1,9 +1,22 @@
 import React from "react"
+import PropTypes from 'prop-types';
 
-export default Loading = ({ children, customClasses }) => (
+const Loading = ({ children, customClasses }) => (
   <div id="loading-overlay">
     <div className={`loading-content ${customClasses}`}>
       {children}
     </div>
   </div>
-)
+);
+
+Loading.propTypes = {
+  children: PropTypes.node,
+  customClasses: PropTypes.string
+};
+
+Loading.defaultProps = {
+  children: null,
+  customClasses: null
+};
+
+export default Loading;
