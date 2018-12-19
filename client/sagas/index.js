7be34bf8582eb8100045f050a71d630f7cee0c2a
Jon Lau <jonhlau28@gmail.com>
2018-12-19 01:35:35 -0800

    CORS issue and type error on dispatch from watcher

---
+++ client/sagas/index.js
@@ -0,0 +1,9 @@
+import lyftAPiWatcher from "./lyft_saga"
+import { all } from 'redux-saga/effects'
+
+
+export default function* rootSaga() {
+    yield all([
+        lyftAPiWatcher,
+    ])
+}
--- server/server.js
+++ server/server.js
@@ -3,10 +3,13 @@ const router = require("./routes/routes.js")
 const path = require("path")
 const bodyParser = require("body-parser")
 const app = express()
+const cors = require('cors')
 
 app.set("views", path.join(__dirname, "../client"))
+app.use(cors())
 app.use(express.static(path.join(__dirname, "../client/resources")))
 app.use(bodyParser.json({ limit: "50mb" }))
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: false }))
 app.use("/", router)
+
 module.exports = app
