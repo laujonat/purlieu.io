44e609b1f192e706d1559612935322b6b469c9bd
Jonathan Lau <jonhlau28@gmail.com>
2019-01-05 01:39:59 -0800

    Refactored express route directories
    * Server did not make sense, renamed to app
    * Routes is not a root directory

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const router = require("./routes")
+const router = require("../routes")
 const path = require("path")
 const bodyParser = require("body-parser")
 const app = express()
