7ab576efd81de244c6db5e6c2fc5f24eccd477f8
Jonathan Lau <jonhlau28@gmail.com>
2019-01-04 21:37:27 -0800

    Snapshot initial render components on ui

---
+++ client/components/UserInterface/UserInterface.test.js
@@ -0,0 +1,16 @@
+import React from "react"
+import NavPane from "../NavPane";
+import Map from "../Map"
+import { shallow } from "enzyme"
+
+describe("UserInterface", () => {
+  it("renders the NavPane with no props", () => {
+    const NavPaneComponent = shallow(<NavPane />)
+    expect(NavPaneComponent).toMatchSnapshot();
+  })
+
+  it("renders the NavPane with no props", () => {
+    const MapComponent = shallow(<Map />)
+    expect(MapComponent).toMatchSnapshot();
+  })
+})
