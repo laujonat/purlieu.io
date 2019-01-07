277bbae5537f1c7e2aaf4a37b7302e17746a5db4
Jonathan Lau <jonhlau28@gmail.com>
2019-01-06 23:12:55 -0800

    Fortify linter rules
    * Addressed prettier and eslint version incompatibility
    * Added eslinter for html files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,23 @@
 module.exports = {
+  extends: ["eslint:recommended", "prettier", "plugin:react/recommended"],
   env: {
     browser: true,
     node: true,
     es6: true,
     jest: true
   },
-  extends: ["eslint:recommended", "prettier", "plugin:react/recommended"],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    allowImportExportEverywhere: true,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      modules: true,
+      spread: true,
+      restParams: true
+    }
+  },
   settings: {
     react: {
       pragma: "React",
@@ -20,25 +32,48 @@ module.exports = {
       mount: true
     }
   },
-  parser: "babel-eslint",
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: "module",
-    allowImportExportEverywhere: true,
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true,
-      modules: true,
-      spread: true,
-      restParams: true
-    }
-  },
   rules: {
+    "no-unused-vars": [
+      1,
+      {
+        argsIgnorePattern: "res|next|^err"
+      }
+    ],
+    "arrow-body-style": [2, "as-needed"],
+    "space-before-function-paren": 0,
+    "comma-dangle": 0,
+    "max-len": 0,
+    "import/extensions": 0,
+    "no-underscore-dangle": 0,
+    "no-console": 0,
+    "react/jsx-filename-extension": [
+      1,
+      {
+        extensions: [".js", ".jsx"]
+      }
+    ],
+    radix: 0,
     camelcase: [
       "error",
       {
         properties: "never"
       }
     ],
-    "no-console": "off"
-  }
+    quotes: [
+      2,
+      "double",
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true
+      }
+    ],
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: false,
+        printWidth: 120
+      }
+    ]
+  },
+  plugins: ["eslint-plugin-html", "prettier"]
 }
