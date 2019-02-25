a029a1e207c7203f6d604b4ae096e478edcfd390
Jonathan Lau <jonhlau28@gmail.com>
2019-02-24 19:04:05 -0800

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
   target: "web",
   plugins: [
     new Dotenv({
-      path: "./.env.local",  // load this now instead of the ones in '.env'
+      path: "./.env", // load this now instead of the ones in '.env'
       safe: true, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
       systemvars: true
     })
