2665af507416a3e3ba9deb92647145b0d6a056da
Jonathan Lau <jonhlau28@gmail.com>
2019-04-22 01:55:32 -0700

    babel remove production for now

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,8 @@ router.get("/", (req, res) => {
 })
 
 router.get("/rideEstimate", (req, res) => {
+  console.log("RIDE ESTIMATES")
+  console.log(process.env.LYFT_CLIENT_ID, process.env.LYFT_CLIENT_SECRET)
   const lyft = new Lyft(process.env.LYFT_CLIENT_ID, process.env.LYFT_CLIENT_SECRET)
 
   const query = {
